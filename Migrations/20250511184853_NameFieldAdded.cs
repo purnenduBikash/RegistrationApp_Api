22e18eee1d2e09acb953fce3757209adbab6b9e4
eleminants <purnendusarangi797@gmail.com>
2025-05-12 01:19:08 +0530

    Changes done on 12-05-2025

---
+++ Migrations/20250511184853_NameFieldAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace authECCApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class NameFieldAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace authECCApi.Models
+{
+    public class AppUser:IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName ="nvarchar(150)")]
+        public string FullName { get; set; }
+    }
+}
