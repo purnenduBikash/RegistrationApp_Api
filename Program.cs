c870f5055f3e309780a558fe9c2a7b204634c4f1
eleminants <purnendusarangi797@gmail.com>
2025-05-20 00:52:10 +0530

    Changes done on 20-05-2025

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using authECCApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +33,20 @@ builder.Services.Configure<IdentityOptions>(options => //to modify the validatio
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DevDB")));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(y =>
+{
+    y.SaveToken = false;
+    y.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:JWTSecret"]!))
+    };
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -39,12 +58,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors(options=>
+app.UseCors(options =>
             options.WithOrigins("http://localhost:4200")
                    .AllowAnyMethod()
                    .AllowAnyHeader()
             );
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -69,6 +89,36 @@ app.MapPost("/api/signup", async (
     else
         return Results.BadRequest(result);
 });
+app.MapPost("/api/signin", async (
+    UserManager<AppUser> userManager,
+    [FromBody] LoginModel LoginModel) =>
+{
+    var user = await userManager.FindByEmailAsync(LoginModel.Email);
+    if (user != null && await userManager.CheckPasswordAsync(user, LoginModel.Password))
+    {
+        var signInKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:JWTSecret"]!));
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim("UserId", user.Id.ToString()),
+                //new System.Security.Claims.Claim("email", user.Email),
+                //new System.Security.Claims.Claim("fullName", user.FullName)
+            }),
+            Expires = DateTime.UtcNow.AddDays(7), //Thala for a reason
+            SigningCredentials = new SigningCredentials(signInKey, SecurityAlgorithms.HmacSha256Signature)
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var sicuriToken = tokenHandler.CreateToken(tokenDescriptor);
+        var token = tokenHandler.WriteToken(sicuriToken);
+        return Results.Ok(new { token });
+    }
+    else
+    {
+        return Results.BadRequest(new { message = "Invalid login attempt" });
+    }
+
+});
 
 app.Run();
 
@@ -77,4 +127,10 @@ public class UserRegistrationModel
     public string Email { get; set; }
     public string Password { get; set; }
     public string FullName { get; set; }
+}
+
+public class LoginModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
